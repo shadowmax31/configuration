e16c233a7cb31046fecc80702826ba291e25b4fe
Etienne Beaulieu <ety_95@hotmail.fr>
2018-10-17 18:12:51 -0400

    Modif

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+config.set("url.searchengines", {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "y": "https://www.youtube.com/results?search_query={}",
+    "r": "https://www.reddit.com/search?q={}"
+});
+
+config.set("editor.command", ["vim", "-f", "{file}", "-c", "normal {line}G{column0}l"]);
