2325e11f0970bf662323d0607216cce7f7311d8d
Etienne Beaulieu <ety_95@hotmail.fr>
2019-03-17 16:38:22 -0400

    Set dmenu color from pywal in confirm script

--- .scripts/confirm.sh
+++ .scripts/confirm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-answer=$(echo -e "Yes\nNo" | dmenu -i -p "$1")
+. "${HOME}/.cache/wal/colors.sh"
+
+answer=$(echo -e "Yes\nNo" | dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -i -p "$1")
 
 if [ "$answer" == "Yes" ]; then
 	$2
